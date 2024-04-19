49f197f74a89a89f6c7a538dafac66c7ab1b10f9
Emmanuel Timothy <66715884+afuk08@users.noreply.github.com>
2024-04-19 15:46:33 +0700

    Update lat11.cpp

--- Latihan/lat11(function)/lat11.cpp
+++ Latihan/lat11(function)/lat11.cpp
@@ -3,9 +3,16 @@
 using namespace std;
 //make a function
 void sayHi(){
-    cout<< "Hello User";
+    cout<< "Hello User" << endl;
+}
+void call(string name,int age){
+    cout << "Hello " << name << " you are " << age << " years old" << endl;
 }
 int main(){
     //call the function
     sayHi();
+    //call the function + give the variable value
+    call("Tom",16);
+    call("Steeve", 38);
+    call("Robert", 79);
 }
