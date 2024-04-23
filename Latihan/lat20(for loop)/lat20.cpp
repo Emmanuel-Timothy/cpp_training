6338a2c6d1aa7e4c5eb020ec89af59a3b1692fa3
Emmanuel Timothy <66715884+afuk08@users.noreply.github.com>
2024-04-23 17:33:18 +0700

    Add files via upload

---
+++ Latihan/lat20(for loop)/lat20.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    for(int i = 1; i <= 5; i++){// the difference between while and for loop is that while loop is not a fixed loop but for loop is a fixed loop
+        cout << i << endl;
+    }
+}
